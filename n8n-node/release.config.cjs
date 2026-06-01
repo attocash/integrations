3ca3923b7ba546ca3f18a2762d91346448b5abdd
rotilho <felipe@rotilho.com>
2026-06-02 00:04:49 +0200

    ci(n8n-node): remove custom releaseRules

--- n8n-node/release.config.cjs
+++ n8n-node/release.config.cjs
@@ -1,23 +1,8 @@
-const n8nScope = "n8n-node";
-
 module.exports = {
 	branches: ["main"],
 	tagFormat: "n8n-node-v${version}",
 	plugins: [
-		[
-			"@semantic-release/commit-analyzer",
-			{
-				releaseRules: [
-					{ breaking: true, scope: n8nScope, release: "major" },
-					{ revert: true, scope: n8nScope, release: "patch" },
-					{ type: "feat", scope: n8nScope, release: "minor" },
-					{ type: "fix", scope: n8nScope, release: "patch" },
-					{ type: "perf", scope: n8nScope, release: "patch" },
-					{ type: "revert", scope: n8nScope, release: "patch" },
-					{ type: "*", release: false },
-				],
-			},
-		],
+		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		[
 			"@semantic-release/github",
